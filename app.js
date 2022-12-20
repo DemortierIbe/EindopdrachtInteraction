93b05f00a49839097ae5bba78cbc3a1889cc642b
DemortierIbe <ibe.demortier@student.howest.be>
2022-12-20 18:38:20 +0100

    data visualisatie

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const listentodog  = function () {
-    let dog = document.querySelectorAll('.js-ikbenkoppig');
+    let dog = document.querySelectorAll('.js-image');
     let kruisje = document.querySelectorAll('.c-kruisje');
   dog.forEach((item, i) => {
-    if(!(document.querySelector('.popup'))) {
+    if(!(document.querySelector('.c-popup'))) {
     item.addEventListener('click', () => {
         console.log("clicked");
+        item.classList.add('c-popup');
         
-        item.classList.add('popup');
         console.log("zet kruisje");
         
         
+
         item.querySelector('.c-kruisje').innerHTML=`<?xml version="1.0" encoding="iso-8859-1"?>
         <!-- Generator: Adobe Illustrator 18.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
         <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
@@ -29,15 +30,18 @@ const listentodog  = function () {
 }
 
 const listentokruisje = function () {
+  console.log(document.querySelector('.c-popup').querySelector('.c-info').getAttribute('weight'));
   let kruisje = document.querySelectorAll('.c-kruisje');
-  let popup = document.querySelector('.popup');
+  let popup = document.querySelector('.c-popup');
   kruisje.forEach((item, i) => {
     item.addEventListener('click', () => {
         console.log("clicked kruisje");
         console.log(item);
         
-        setTimeout(function(){ popup.classList.remove('popup'); item.innerHTML=""; console.log("kruisje leeg");}, 100);
+        setTimeout(function(){ popup.classList.remove('c-popup'); item.innerHTML=""; console.log("kruisje leeg");}, 100);
+        
     })
+    
   })
 };
 
@@ -61,13 +65,23 @@ const getData = (endpoint) => {
   let showFoto = function (dogs) {
     let doghtml = '';
     for (let dog of dogs) {
+      const result = dog.weight.metric.split(' - ');
+      console.log(result[1]);
+      const maxgewicht = result[1] - result[0];
+      const mingewicht = result[0];
       
       doghtml += `
-      <div class="c-rectangle gallery-image js-ikbenkoppig">
-        <div class="c-info">
+      <div class="c-rectangle c-gallery-image js-image" tabindex=0>
+        <div class="c-info" weight="${dog.weight.metric}">
           <img  src="${dog.image.url}" alt="" class="c-image">
           <p class="c-name_dogs">${dog.name}</p>
           <p class="c-extrainfo">${dog.weight.metric}</p>
+          <div class="c-volledigebar">
+          <div class="c-eerstedeel-bar" style="width:${mingewicht}%;">
+            </div>
+            <div class="c-green-bar" style="width:${maxgewicht}%;">
+            </div>
+          </div>
           <p class="c-extrainfo">${dog.temperament}</p>
           <p class="c-extrainfo">${dog.origin}</p>
           <p class="c-extrainfo">${dog.life_span}</p>
